69cc069a8f52ed0d1ba7c716c4a4b02fee775b7d
Denis Veselov <progr-m@yandex.ru>
2012-10-30 01:03:27 +0400

    add base class for mso files

---
+++ mso.py
@@ -0,0 +1,21 @@
+import zipfile
+import lxml
+
+
+class MSOFile(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def is_zipfile(self):
+        return zipfile.is_zipfile(self.filename)
+
+    def read(self):
+        raise NotImplementedError(
+            'This method must be implemented by subclasses'
+        )
+
+    def get_text(self):
+        raise NotImplementedError(
+            'This method must be implemented by subclasses'
+        )
